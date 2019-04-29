a1b0f84bef4442c815e73546b0a6cee4cdced07b
Franchely Portorreal <franchely811@gmail.com>
2019-04-29 04:21:56 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = [
+  "snickers",
+  "hundred grand",
+  "kitkat",
+  "skittles"];
+  
+  function addElementToBeginningOfArray(array, element) {
+  var moreChocolateBars = [element, ...array]
+   return  moreChocolateBars
+  }
+  
+  function destructivelyAddElementToBeginningOfArray(array, element) {
+    array.unshift(element) 
+    return array 
+  }
+  
+  function addElementToEndOfArray(array, element) {
+    var moreChocolateBars = [...array, element]
+    return moreChocolateBars
+  }
+  function destructivelyAddElementToEndOfArray(array, element) {
+    array.push(element)
+    return array
+  }
+  
+  function accessElementInArray(array, index) {
+    return 
+  }
+  
+  
